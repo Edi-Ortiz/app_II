2e78acec38f3c0f4ba92e73ae3695be2c39ab32c
Edi-Ortiz <61667880+Edi-Ortiz@users.noreply.github.com>
2020-12-09 10:48:06 -0600

    creacion de pantallas
    se creo todas las pantallas funcionales con la navegacion.

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import paginaPrincipal from "./src/screens/paginaPrincipal";
+import paginaNavegacion from "./src/screens/paginaNavegacion";
+import opciones from "./src/screens/opciones";
+import toplist from "./src/screens/toplist";
+import agregar from "./src/screens/agregar";
+import eliminar from "./src/screens/eliminar";
+import modificar from "./src/screens/modificar";
 
 const Stack = createStackNavigator();
 
@@ -10,6 +16,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Music Top List">
       <Stack.Screen name="Music Top List" component={paginaPrincipal} />
+      <Stack.Screen name="Navegacion" component={paginaNavegacion} />
+      <Stack.Screen name="Opciones" component={opciones} />
+      <Stack.Screen name="Agregar" component={agregar} />
+      <Stack.Screen name="Top List" component={toplist} />
+      <Stack.Screen name="Modificar" component={modificar} />
+      <Stack.Screen name="Eliminar" component={eliminar} />
       </Stack.Navigator>
     </NavigationContainer>
   )
